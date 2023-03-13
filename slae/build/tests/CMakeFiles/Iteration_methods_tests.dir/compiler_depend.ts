48d4acc0f9ab868672c3d9bc5ea4452d7e713a73
Esaak <gerasimov.ia@phystech.edu>
2023-03-13 23:14:08 +0300

    fix bags in CSR matrix

--- slae/build/tests/CMakeFiles/Iteration_methods_tests.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for Iteration_methods_tests.
--- slae/build/tests/Google_tests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ilya/slae_lab/slae/tests/Google_tests/lib/googletest
-# Build directory: /home/ilya/slae_lab/slae/build/tests/Google_tests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
