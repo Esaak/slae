8a3b1b4ab14dc10fa86193e550b1ad0efefb2482
Esaak <gerasimov.ia@phystech.edu>
2023-04-08 22:07:54 +0300

    add py_tests

---
+++ slae/cmake-build-debug-coverage/tests/Google_tests/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ilya/slae_lab/slae/tests/Google_tests/lib
+# Build directory: /home/ilya/slae_lab/slae/cmake-build-debug-coverage/tests/Google_tests/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ slae/cmake-build-debug-coverage/tests/Google_tests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ilya/slae_lab/slae/tests/Google_tests/lib/googletest
+# Build directory: /home/ilya/slae_lab/slae/cmake-build-debug-coverage/tests/Google_tests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
