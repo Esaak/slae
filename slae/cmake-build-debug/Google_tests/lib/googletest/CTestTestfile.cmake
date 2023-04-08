6dae0ea9eaac5be2f4eacdb6b4d20f5128df0526
Esaak <gerasimov.ia@phystech.edu>
2023-04-08 22:18:58 +0300

    -a

--- slae/cmake-build-debug/Google_tests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ilya/slae_lab/slae/Google_tests/lib/googletest
-# Build directory: /home/ilya/slae_lab/slae/cmake-build-debug/Google_tests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
